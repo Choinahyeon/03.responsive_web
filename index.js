2911f28aa207d9522b62e623bea58d073edf485e
Choinahyeon <skgusdlekt@gmail.com>
2022-03-16 18:05:16 +0900

    Initial commit

--- index.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navbar .navbar_toggleBtn');
-const menu = document.querySelector('.navbar .navbar_menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
